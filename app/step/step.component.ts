7d9dc7319d8fb3b53337e9a2c803fdfd704ac249
Gemoli Domenico <tech737@yoox.com>
2016-11-28 12:16:29 +0100

    Basic parameter styling

--- app/step/step.component.ts
+++ app/step/step.component.ts
@@ -3,10 +3,10 @@ import { Component, Input} from '@angular/core';
 import { Step } from './step';
 
 @Component({
+    moduleId: module.id.replace('/dist/', '/'),
     selector: 'step',
-    template: `
-      <mark>{{step.type}}</mark> <span *ngFor="let parameter of step.parameters">{{parameter.value}}</span>
-    `,
+    styleUrls: ['step.component.css'],
+    templateUrl: 'step.component.html',
 })
 export class StepComponent {
     @Input() public step: Step;
